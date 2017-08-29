083f1e26f6a84e2abef3ec5994763614e9f01033
Nikolays93 <nikolayS93@ya.ru>
2017-08-29 17:58:30 +0400

    fix textarea id

--- assets/project-settings.js
+++ assets/project-settings.js
@@ -1,8 +1,4 @@
-pcjq = jQuery.noConflict();
-pcjq(function( $ ) {
-  // onload
-  $(function(){
-
+jQuery(document).ready(function($) {
     // Считаем нажатые глазки
     function compileResult(){
       var result = '';
@@ -11,7 +7,7 @@ pcjq(function( $ ) {
           result += $(this).parent().children('a').attr('href') + ',';
         }
       });
-      $('input#menu').val(result);
+      $('input#globals_menu').val(result);
 
       var result = '';
       $('#adminmenu>li>ul span.after').each(function(){
@@ -22,7 +18,7 @@ pcjq(function( $ ) {
           result += parent + '>' + obj +',';
         }
       });
-      $('input#sub_menu').val(result);
+      $('input#globals_sub_menu').val(result);
     }
 
     // Добавить шестерни
@@ -32,7 +28,7 @@ pcjq(function( $ ) {
         elem[1] = elem[1] ? elem[1].replace('_', '-') : 'post-type=post';
 
         link = '/wp-admin/options-general.php?page=' + menu_disabled.edit_cpt_page + '&' + elem[1];
-        $(this).parent('li').append( 
+        $(this).parent('li').append(
           $("<a></a>").attr('href', link).attr('class', 'after dashicons dashicons-admin-generic') );
       }
     });
@@ -42,7 +38,7 @@ pcjq(function( $ ) {
       if(!$(this).hasClass('wp-menu-separator') && $(this).attr('id') != 'collapse-menu' && !$(this).hasClass('hide-if-no-customize')){
         $(this).append('<span class="after dashicons dashicons-hidden"></span>');
       }
-    });      
+    });
 
     var mainMenus = menu_disabled.menu.split(',');
     var subMenus = menu_disabled.sub_menu.split(',');
@@ -61,7 +57,7 @@ pcjq(function( $ ) {
       return false;
     });
 
-    // Добавить куку
+    // Toogle Cookie
     $('#setNotHide').on('click', function(){
       $(this).toggleClass('button-primary');
 
@@ -73,8 +69,8 @@ pcjq(function( $ ) {
         document.cookie = "developer=; path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT;";
       }
     });
-  
-    // Placeholder to value  
+
+    // Placeholder to value
     $('input[type=\'text\'], input[type=\'number\'], textarea').on('focus', function(){
       var ph = $(this).attr('placeholder');
       if($(this).val() == '' && typeof(ph) == 'string' ){
@@ -90,20 +86,31 @@ pcjq(function( $ ) {
       $('[name="_wp_http_referer"]').val( referer + type );
     });
 
-    var singleSelector = 'input#labels_singular_name';
-    var pluralSelector = 'input#label';
-    $( singleSelector + ', ' + pluralSelector).on('keyup change focus', function(event) {
-      var single = $(singleSelector).val();
-      var plural = $(pluralSelector).val();
+    var patterns = [
+      { selector : 'input#post_type_name', pattern : '[id]' },
+      { selector : 'input#labels_singular_name', pattern : '[singular]' },
+      { selector : 'input#labels_name', pattern : '[plural]' },
+      { selector : 'input#labels_name_admin_bar', pattern : '[accusative]' }
+    ];
 
-      $("input[data-pattern]").each(function(index, el) {
-        if( ! $(this).val() )
-          $(this).attr('data-fill-pattern', 1);
+    patterns.forEach(function(item, i, arr) {
+      $(item.selector).on('keyup change focus', function(event) {
+        var template = $(this).val();
 
-        if($(this).attr('data-fill-pattern'))
-          $(this).val( $(this).attr('data-pattern').replace("[single]", single).replace("[plural]", plural) );
+        $("input[data-pattern]").each(function(index, el) {
+          if( ! $(this).val() )
+            $(this).attr('data-fill-pattern', 1);
+
+          if($(this).attr('data-fill-pattern')){
+            var pat = $(this).attr('data-pattern');
+
+            if( pat.indexOf(item.pattern) == 0 )
+              $(this).val(pat.replace(item.pattern, template) );
+
+            else if( pat.indexOf(" " + item.pattern) >= 0 )
+              $(this).val( pat.replace(" " + item.pattern, " " + template.toLowerCase() ) );
+          }
+        });
       });
     });
-
-  });
 });
